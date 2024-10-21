9051540bc221aed28673a13f01e0a23a487b82a7
alaminsaleh <abusalehalamin52@gmail.com>
2024-10-21 17:38:15 +0600

    explore-react-core-concepts-module39-part2-recap

---
+++ src/Post.jsx
@@ -0,0 +1,14 @@
+import './Post.css'
+
+export default function Post ({post}){
+   const {title, body, id, userId} =post;
+    return(
+        <div className='post'>
+            <h5>Title: {title}</h5>
+            <p><small>userID: {userId}</small></p>
+            <p><small>postID: {id}</small></p>
+            <p>{body}</p>
+        </div>
+    )
+}
+
